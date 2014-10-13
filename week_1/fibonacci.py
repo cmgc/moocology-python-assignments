04fd6156729b655d36cba4581b4716d40fe5e7f5
1fox <fut4us@gmail.com>
2014-10-13 10:32:41 -0400

    fix up

---
+++ week_1/fibonacci.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# for positive only
+def fibonacci(n):
+  if n < 0:
+      print "Please enter value greater than zero"
+      return
+  if n == 0:
+      return 0
+
+  prelast, last = 0, 1
+  i = 0
+
+  while(i < n):
+      last, prelast = prelast, last + prelast
+      print last 
+      i += 1
+
+fibonacci(10)
+fibonacci(5)
